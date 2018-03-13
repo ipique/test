862fcb8b7b4848bc83abc3414fc1bd83bf4539ed
ipique <ipique888@gmail.com>
2018-03-14 01:59:18 +0300

    uploading

---
+++ test.py
@@ -0,0 +1,18 @@
+import telebot
+from telebot import types
+
+token = "512605692:AAFFP-vV8mD_If52Yd-UEEIci0IV6gsC3XU"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+        bot.send_message(message.from_user.id, "Psss.. \nDo you want me to kinkle you some great stuff?"
+                                               "\nUse command /getsignal to be in a know.")
+
+@bot.message_handler(commands=["getsignal"])
+def getsignal(message):
+        key = telebot.types.InlineKeyboardMarkup()
+        button_1 = telebot.types.InlineKeyboardButton(text="Payment",url="https://google.com")
+        key.add(button_1)
+        bot.send_message(message.from_user.id, "You can just copy address 0xf4723db6a2655ff89ca4a39c4683d94c1b046630 and pay via your wallet, or use API", reply_markup=key)
